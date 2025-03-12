077e84aa8b6b31bd95163b34124ac876dcd481b8
luii2606 <valemidx@gmail.com>
2025-03-11 20:52:05 -0500

    intento dos

--- Ejercicio6/ejercicio6.js
+++ Ejercicio6/ejercicio6.js
@@ -1,1 +1,16 @@
-let archivo = await fetch('')
+// const archivo = await fetch('https://jsonplaceholder.typicode.com/users')
+//   .then(response => response.json())
+//   .then(json => console.log(json));
+
+  // const usuarios= await fetch('')
+
+  // function fetchUsers() {
+  //   return fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json());
+
+// const users = async () => {
+  
+// }
+async function fetchUsers() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    return response.json();
+}
